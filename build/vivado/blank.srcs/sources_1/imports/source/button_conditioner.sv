8724a6f0024971a883bdab6cc31c7ac2477c114a
Ye Ye Taut <38886560+yeyetaut@users.noreply.github.com>
2025-04-13 19:53:12 +0800

    testing Shift Registers

--- build/vivado/blank.srcs/sources_1/imports/source/button_conditioner.sv
+++ build/vivado/blank.srcs/sources_1/imports/source/button_conditioner.sv
@@ -13,12 +13,12 @@ module button_conditioner #(
         input wire in,
         output reg out
     );
-    localparam _MP_DEPTH_1536080584 = NUM_SYNC;
+    localparam _MP_DEPTH_26487910 = NUM_SYNC;
     logic M_sync_in;
     logic M_sync_out;
     
     pipeline #(
-        .DEPTH(_MP_DEPTH_1536080584)
+        .DEPTH(_MP_DEPTH_26487910)
     ) sync (
         .clk(clk),
         .in(M_sync_in),
